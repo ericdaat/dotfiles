32d24276b99dc9fbee7ace6a3cc50d3b9a326842
Eric Daoud <daoudattoyan.eric@gmail.com>
2019-08-22 16:04:44 +0200

    add new brew cask installs

--- brew-cask.sh
+++ brew-cask.sh
@@ -2,11 +2,18 @@
 
 brew install caskroom/cask/brew-cask;
 
+# Browsers
+brew cask install google-chrome;
+brew cask install firefox;
+
+# Code
 brew cask install postman;
 brew cask install visual-studio-code;
+brew cask install sublime-text;
 brew cask install iterm2;
 brew cask install hyper;
-brew cask install sublime-text;
+brew cask install docker;
 
+# Media
 brew cask install spotify;
 brew cask install vlc;
