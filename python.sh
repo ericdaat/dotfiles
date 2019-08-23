c4768e7d1f9fd1425cef4ad2625f727981eae23e
Eric Daoud <daoudattoyan.eric@gmail.com>
2019-08-23 12:03:07 +0200

    jupyterlab extensions

--- python.sh
+++ python.sh
@@ -6,5 +6,11 @@ pip install jupyterlab;
 virtualenv venv;
 source venv/bin/activate;
 
+# Jupyter Lab
+## Kernel
 pip install ipykernel;
 python -m ipykernel install --user --name myenv --display-name "Python (venv)"
+
+## Extensions
+jupyter labextension install @jupyterlab/toc
+
