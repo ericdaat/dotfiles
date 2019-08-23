04f78fad1389c4bc6c439f13316e2dc0f11aba35
Eric Daoud <daoudattoyan.eric@gmail.com>
2019-08-23 13:21:18 +0200

    more jupyter extensions

--- python.sh
+++ python.sh
@@ -9,8 +9,11 @@ source venv/bin/activate;
 # Jupyter Lab
 ## Kernel
 pip install ipykernel;
+pip install autopep8;
 python -m ipykernel install --user --name myenv --display-name "Python (venv)"
 
 ## Extensions
 jupyter labextension install @jupyterlab/toc
+jupyter labextension install @krassowski/jupyterlab_go_to_definition
+pip install jupyterlab_latex && jupyter labextension install @jupyterlab/latex
 
