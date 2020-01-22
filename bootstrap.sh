d45020c810fa601bf46986389034b23032bfcb6e
Eric Daoud <daoudattoyan.eric@gmail.com>
2020-01-22 18:07:31 +0100

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,45 +4,32 @@
 xcode-select --install
 
 # fonts
-cp fonts/SourceCodePro-Regular.ttf ~/Library/Fonts/
+cp fonts/SourceCodePro-Regular.ttf $HOME/Library/Fonts/
 
 # Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 brew update;
 brew upgrade;
-bash brew/brew-cask.sh
-bash brew/brew.sh
+bash brew/brew-cask.sh;
+bash brew/brew.sh;
 
 # Configure vim
-bash vim/vim.sh
+bash vim/vim.sh;
 
 # Install & configure python
-bash python/python.sh
+bash python/python.sh;
 
 # zsh customization
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  # ohmyzsh
-
-## zsh theme
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-## zsh dotfiles
-cp zsh/.zshrc ~/
-cp zsh/.aliases.zsh ~/
-cp zsh/.fzf.zsh ~/
-cp zsh/.p10k.zsh ~/
+bash zsh/zsh.sh;
 
 # vscode
-cp vscode/settings.json ~/Library/Application Support/Code/User/settings.json
+bash vscode/vscode.sh;
 
 # git
-cp git/.gitconfig ~/.gitconfig
-
-# jupyter
-
+cp git/.gitconfig $HOME/.gitconfig;
 
 # Sublime Text
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin  # sublime text
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin;
 
 # Other
 cp .hyper.js ~/
