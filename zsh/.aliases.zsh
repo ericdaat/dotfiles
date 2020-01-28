14437b41cd4d425c590182858fef9040dddcab81
Eric Daoud <daoudattoyan.eric@gmail.com>
2020-01-28 16:39:26 +0100

    edit aliases

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -1,12 +1,13 @@
 alias pip='pip3'
 alias python='python3'
-alias vdata='source ~/Code/virtualenvs/data/bin/activate'
+alias vdata='source $HOME/Code/virtualenvs/data/bin/activate'
 
+alias tjup='tmux new -d -s jupyter "jupyter-lab --no-browser"';
 alias tas='tmux attach-session -t'
+
 alias mkcd='foo(){ mkdir -p "$1"; cd "$1" }; foo '
 alias svim='sudo vim'
 alias vi='vim'
 alias c='clear'
 
 alias ipi='ipconfig getifaddr en0'
-
