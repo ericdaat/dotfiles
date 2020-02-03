91647a574b65b1bde61462ea6178278788b598d0
Eric Daoud <daoudattoyan.eric@gmail.com>
2020-02-03 11:10:37 +0100

    minor changes

--- sublime_text/README.md
+++ sublime_text/README.md
@@ -1,9 +1,1 @@
 # Sublime Text packages
-
-- Emmet
-- Material Theme
-- Package Control
-- SideBarEnhancements
-- SublimeLinter-flake8
-- SublimeLinter-pep8
-- SummitLinter
--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -1,8 +1,8 @@
-alias pip='pip3'
-alias python='python3'
+# alias pip='pip3'
+# alias python='python3'
 alias vdata='source $HOME/Code/virtualenvs/data/bin/activate'
 
-alias tjup='tmux new -d -s jupyter "jupyter-lab --no-browser"';
+alias jupyter-tmux='tmux new -d -s jupyter "jupyter-lab --no-browser"';
 alias tas='tmux attach-session -t'
 
 alias mkcd='foo(){ mkdir -p "$1"; cd "$1" }; foo '
