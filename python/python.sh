2bbd1e5925f99a0d47269f40e626380fc1a7dab3
Eric Daoud <daoudattoyan.eric@gmail.com>
2020-02-06 15:03:41 +0100

    python requirements

--- python/python.sh
+++ python/python.sh
@@ -10,3 +10,10 @@ python3 -m pip install pylint flake8 pycodestyle autopep8;
 python3 -m pip install --user --upgrade setuptools wheel;
 python3 -m pip install --user --upgrade twine;
 
+# install requirements in a virtualenv
+virtualenv venv -p python3;
+source venv/bin/activate;
+python3 -m pip install requirements.txt;
+
+# fix matplotlib bug
+cp matplotlibrc $HOME/.matplotlib/
