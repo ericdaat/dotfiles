5d75ff43f20f47c56f4ac96ba9e2b398c216874e
Eric Daoud <ericdaat@users.noreply.github.com>
2020-02-27 06:03:46 -0800

    Update python.sh

--- python/python.sh
+++ python/python.sh
@@ -11,9 +11,9 @@ python3 -m pip install --user --upgrade setuptools wheel;
 python3 -m pip install --user --upgrade twine;
 
 # install requirements in a virtualenv
-virtualenv venv -p python3;
-source venv/bin/activate;
-python3 -m pip install -r requirements.txt;
+virtualenv $HOME/venv -p python3;
+source $HOME/venv/bin/activate;
+python3 -m pip install -r python/requirements.txt;
 
 # fix matplotlib bug
 cp matplotlibrc $HOME/.matplotlib/
