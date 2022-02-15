d0dd9827677cd390506dad38884b98bad642c275
Eric Daoud <ericdaat@users.noreply.github.com>
2022-02-15 17:47:25 +0100

    new cask syntax

--- homebrew/brew-cask.sh
+++ homebrew/brew-cask.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 # Browsers
-brew cask install google-chrome;
-brew cask install firefox;
+brew install --cask google-chrome;
+brew install --cask firefox;
 
 # Code
-brew cask install postman;
-brew cask install visual-studio-code;
-brew cask install sublime-text;
-brew cask install sublime-merge;
-brew cask install iterm2;
-brew cask install hyper;
-brew cask install docker;
-brew cask install mactex;
-brew cask install homebrew/cask-versions/adoptopenjdk8;
-brew cask install rstudio;
-brew cask install metabase;
+brew install --cask postman;
+brew install --cask visual-studio-code;
+brew install --cask sublime-text;
+brew install --cask sublime-merge;
+brew install --cask iterm2;
+brew install --cask hyper;
+brew install --cask docker;
+brew install --cask mactex;
+brew install --cask homebrew/cask-versions/adoptopenjdk8;
+brew install --cask rstudio;
+brew install --cask metabase;
 
 # Apps
-brew cask install slack;
-brew cask install spotify;
-brew cask install vlc;
-brew cask install skype;
-brew cask install zoomus;
-brew cask install whatsapp;
-brew cask install zotero;
-brew cask install google-backup-and-sync;
+brew install --cask slack;
+brew install --cask spotify;
+brew install --cask vlc;
+brew install --cask skype;
+brew install --cask zoomus;
+brew install --cask whatsapp;
+brew install --cask zotero;
+brew install --cask google-backup-and-sync;
 
 # Others
-brew cask install chromedriver
-brew cask install xquartz;
+brew install --cask chromedriver
+brew install --cask xquartz;
